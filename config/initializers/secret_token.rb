862a7484df316f8c04d6210ee363df219bd2457d
kern <kern86@gmail.com>
2010-08-05 11:50:09 +0800

    initial push (rails skeleton)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rpress::Application.config.secret_token = '9e087650ef6cfb5ff21c691ce2c3ea25c3734cb5639315517f95281b240e0bae2aeae24ad4eee29badf6bec1b5e118c5eae785aa06773838869a21749e68ce60'
